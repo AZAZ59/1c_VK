9022bbb40402235acdd2a4dceb550b59062a61b0
Sergey Stepanov <stepanov.s.ig@sberbank.ru>
2021-02-11 10:43:58 +0400

    new format for group_id = -202009856

--- downloader_GUI.py
+++ downloader_GUI.py
@@ -12,8 +12,8 @@ session = vk.Session('c297dec325e43588e5472b7331c343760072c052abe7a0c90725f86e7b
 #        stambul     - 'aef44be9f894721156bd392b40d79cd7c51fa6542d63980c04ee9a62a26c16b728819626b3d03c5f41cf4'
 session = vk.Session('aef44be9f894721156bd392b40d79cd7c51fa6542d63980c04ee9a62a26c16b728819626b3d03c5f41cf4')
 
+from utils import VK_Item, merge_excel, art_split_markers, split_marker, date_XX_XX_XXXX, safelist
 
-from utils import VK_Item, merge_excel, art_split_markers, split_marker, date_XX_XX_XXXX,safelist
 
 def process_photo_batch(photos):
     batch_items = []
@@ -22,6 +22,7 @@ def process_photo_batch(photos):
 
         batch_items.append(VK_Item(urls[0]['url'], photo['text'], f'https://vk.com/photo{photo.owner_id}_{photo.id}'))
     return batch_items
+
 
 def extract_art_and_data(raw_data):
     rows = []
@@ -54,96 +55,126 @@ def extract_art_and_data(raw_data):
                 data_cols.append(val)
     return data_cols, rows
 
+
 def extract_correnct_art_and_name(art: str):
     for word in art_split_markers:
         if word in art.lower():
             split_ind = art.lower().index(word)
-            ret_art   = art[:split_ind]
-            ret_name  = art[split_ind:]
+            ret_art = art[:split_ind]
+            ret_name = art[split_ind:]
             return ret_art, ret_name
     ### Если не нашли ни одного слова
 
     error_out.write(' Не найден тип товара - ' + art + '\n')
-#   art_out.flush()
+    #   art_out.flush()
     return art, '________NOT_FOUND__________'
 
 
 def process_to_1c(df, save_dir, name, name_album):
     df2 = pd.DataFrame(columns=[
-            "Картинка",
-            "Ссылка на товар",
-            "Номенклатура",
-            "Наименование полное",
-            'Состав',
-            "Вид номенклатуры",
-            'Группа',
-            'Наименование',
-            'Описание',
-            'Размер',
-            'Фото',
-            'Цена'
+        "Картинка",
+        "Ссылка на товар",
+        "Номенклатура",
+        "Наименование полное",
+        'Состав',
+        "Вид номенклатуры",
+        'Группа',
+        'Наименование',
+        'Описание',
+        'Размер',
+        'Фото',
+        'Цена'
     ])
 
     global error_out
     error_out = open(f'./File/' + date_XX_XX_XXXX + '/errors/' + name + '.txt', 'w', encoding='utf-8')
-  
-    for ind, row in tqdm(df.iterrows(), total=len(df),desc=f'___{name_album}___'):
+
+    for ind, row in tqdm(df.iterrows(), total=len(df), desc=f'___{name_album}___'):
 
         raw_data = str(row['description']).split('\n')
-        raw_data=safelist(raw_data)
-        data_cols, rows = extract_art_and_data(raw_data)
+        raw_data = safelist(raw_data)
 
-        # из первой строки Описания формиуем Наименование полное - текст до "размера" 
-        if len(rows)==0:
-            continue
-        art = rows[0]
+        if group_id != -202009856:
+            ##Старый формат
+            data_cols, rows = extract_art_and_data(raw_data)
+            if len(rows) == 0:
+                continue
+            art = rows[0]
 
-        if ' р ' in art:
-            art, size = str(art).split(' р ')
-        art = art.replace('?', '').strip()
+            if ' р ' in art:
+                art, size = str(art).split(' р ')
+            art = art.replace('?', '').strip()
 
-        # Артикул - это текст Наименование полное до "джемпер"
-        art_new, name_new = extract_correnct_art_and_name(art)
+            # может вынести формирование состава
+            #        print(len(raw_data))
+            df2 = df2.append(extract_property_old(art, data_cols, name_album, raw_data, row), ignore_index=True)
+        else:
+            df2 = df2.append(extract_property_new(name_album, raw_data, row), ignore_index=True)
 
-        # может вынести формирование состава 
-#        print(len(raw_data))
-        df2 = df2.append({
-            "Картинка"           : row['photo_url'],
-            "Ссылка на товар"    : row['link'],
-            "Номенклатура"       : art_new,
-            "Наименование полное": art,
-            'Состав'             : ((data_cols[-3] + " ") if len(data_cols) >= 3 else "") + (
-                    data_cols[-2] if len(data_cols) >= 2 else ""),
-            "Вид номенклатуры"   : name_album,
-            'Группа'             : '02_дек ' + name_album,
-            'Наименование'       : art,
-            'Описание'           : ( 'Артикул: '  + art         + '\n' 
-                                   + 'Описание: ' + raw_data[2] + '\n' 
-                                   + raw_data.get(3,"")                + '\n' 
-				   + raw_data.get(4,"")
-                                   ),
-            'Размер'             : raw_data.get(5,"Нет размера"),
-            'Фото'               : row['photo_url'],
-            'Цена'               : raw_data.get(6,0)
-        }, ignore_index=True)
+    # вывести дубли
+    duplicated = df2[df2.duplicated(subset='Номенклатура')]
+    dupli_out.write(duplicated.to_csv(None, sep=';'))
 
-    # вывести дубли 
-    duplicated=df2[df2.duplicated(subset='Номенклатура')]
-    dupli_out.write(duplicated.to_csv(None,sep=';'))
- 
     # убрать дубли по art_new  -- аритикул --
     df2 = df2.drop_duplicates(subset='Номенклатура')
-    
-    print('\n'*1)
+
+    print('\n' * 1)
 
     error_out.close()
-#   art_out.close()
+    #   art_out.close()
 
     if len(df2) == 0:
         return
     else:
         df2.to_excel(f'{save_dir}/_processed_{name}.xlsx')
-    return           f'{save_dir}/_processed_{name}.xlsx'
+    return f'{save_dir}/_processed_{name}.xlsx'
+
+
+def extract_property_old(art, data_cols, name_album, raw_data, row):
+    # Артикул - это текст Наименование полное до "джемпер"
+    art_new, name_new = extract_correnct_art_and_name(art)
+    return {
+        "Картинка": row['photo_url'],
+        "Ссылка на товар": row['link'],
+        "Номенклатура": art_new,
+        "Наименование полное": art,
+        'Состав': ((data_cols[-3] + " ") if len(data_cols) >= 3 else "") + (
+            data_cols[-2] if len(data_cols) >= 2 else ""),
+        "Вид номенклатуры": name_album,
+        'Группа': '02_дек ' + name_album,
+        'Наименование': art,
+        'Описание': ('Артикул: ' + art + '\n'
+                     + 'Описание: ' + raw_data[2] + '\n'
+                     + raw_data.get(3, "") + '\n'
+                     + raw_data.get(4, "")
+                     ),
+        'Размер': raw_data.get(5, "Нет размера"),
+        'Фото': row['photo_url'],
+        'Цена': raw_data.get(6, 0)
+    }
+
+
+def extract_property_new(name_album, raw_data, row):
+    art = raw_data[0]
+    return {
+        "Картинка": row['photo_url'],
+        "Ссылка на товар": row['link'],
+        'Группа': '02_дек ' + name_album,
+        "Вид номенклатуры": name_album,
+        'Фото': row['photo_url'],
+
+        'Наименование': art,
+        "Номенклатура": art,
+        "Наименование полное": art,
+
+        'Состав': raw_data[2],
+        'Описание': ('Артикул: ' + art + '\n'
+                     + 'Описание: ' + raw_data[3] + '\n'
+                     + 'Цвет:' + raw_data[1]
+                     ),
+        'Размер': raw_data[4],
+        'Цена': raw_data[5]
+    }
 
 
 def download_vk_album(group_id, album_id, save_dir):
@@ -158,10 +189,10 @@ def download_vk_album(group_id, album_id, save_dir):
 
     result_files = []
 
-    for album in tqdm(albums,desc="__ALBUMS__"):
+    for album in tqdm(albums, desc="__ALBUMS__"):
         album_items = []
         offset = 0
-        count  = 1000
+        count = 1000
         photos = api.photos.get(owner_id=group_id, album_id=album['id'], count=1000, photo_sizes=1, offset=offset)
         while (offset + count <= photos['count']):
             album_items.extend(process_photo_batch(photos))
@@ -184,10 +215,9 @@ def download_vk_album(group_id, album_id, save_dir):
 
 
 if __name__ == '__main__':
-
     group_id = -182912257
-#               198234557 ФРЕШ (фото клевер)
-#               202009856 1С клевер
+    #               198234557 ФРЕШ (фото клевер)
+    #               202009856 1С клевер
     group_id = -202009856
 
     album_id = ''
